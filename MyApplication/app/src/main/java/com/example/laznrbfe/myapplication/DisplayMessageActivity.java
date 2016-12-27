206c997be271b4ea75097899a012938752dbd65d
laznrbfe <laznrbfe@zhongtaolianzhongtaoliandeMacBook-Pro.local>
2016-12-27 22:02:26 +0800

    Menu

--- MyApplication/app/src/main/java/com/example/laznrbfe/myapplication/DisplayMessageActivity.java
+++ MyApplication/app/src/main/java/com/example/laznrbfe/myapplication/DisplayMessageActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,11 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // 如果你的minSdkVersion属性是11或者更高，应该用
+        // getActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
 
@@ -38,12 +44,23 @@ public class DisplayMessageActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
         }
-        return super.onOptionsItemSelected(item);
+        else if (id == R.id.action_search) {
+            return true;
+        }
+        else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 
     public static class PlaceholderFragment extends Fragment {
--- MyApplication/app/src/main/java/com/example/laznrbfe/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/laznrbfe/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -66,10 +67,24 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            openSettings();
             return true;
         }
+        else if (id == R.id.action_search) {
+            openSearch();
+            return true;
+        }
+        else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void openSettings() {
+        Log.i("zqm","openSearch");
+    }
+
+    private void openSearch() {
+        Log.i("zqm","openSearch");
     }
 
     /**
